5438a49218c7bf99a0193955c40c91196d80a32f
Aaron Spencer <aaronspencer@softwire.com>
2021-11-01 15:37:37 +0000

    A basic View model controller set up.Gl hf

---
+++ Bookish/Controllers/BooksController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Bookish.Models;
+
+namespace Bookish.Controllers
+{   
+    [Route("/books")]
+    public class BooksController : Controller
+    {
+        private readonly ILogger<BooksController> _logger;
+
+        public BooksController(ILogger<BooksController> logger)
+        {
+            _logger = logger;
+        }
+        
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            var viewModel = new BooksViewModel();
+            return View(viewModel);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
---
+++ Bookish/Models/BooksViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Bookish.Models
+{
+    public class BooksViewModel
+    {
+        public List<string> books = new List<string> {"Thinking Fast and Slow", "Alien", "Sapiens"};
+        
+    }
+}
