393062abc69e6f850eee8e011670ddd5cc627592
Besar <besar.krasniqi@softwire.com>
2021-11-04 12:25:07 +0000

    Added in the other fields

--- Bookish/Models/Database/Book.cs
+++ Bookish/Models/Database/Book.cs
@@ -1,8 +1,14 @@
-﻿namespace Bookish.Models.Database
+﻿using System;
+
+namespace Bookish.Models.Database
 {
     public class Book
     {
         public int id { get; set; }
         public string Title{ get; set; }
+        public int Year { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        
     }
 }
